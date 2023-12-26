4f7b7a6e6041b323434b7671cf289b5d2a2406a8
angeliprd <145030482+angeliprd@users.noreply.github.com>
2023-12-26 23:23:31 +0700

    App.java

--- src/App.java
+++ src/App.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 
 public class App {
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost/Angel'smart";
+    static final String DB_URL = "jdbc:mysql://localhost/minimarket";
     static final String USER = "root";
-    static final String PASS = "1994";
+    static final String PASS = "";
     
 
     static Connection conn;
